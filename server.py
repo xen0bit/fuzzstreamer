d7ff97900ce7c840e5d5a3ba76241bb844ae9c49
xen0bit <xenobitbugs@outlook.com>
2019-04-18 21:40:27 -0400

    first push
    Functions chunked encoding server fuzzed with domato

---
+++ server.py
@@ -0,0 +1,223 @@
+""" 
+Example Python 2.7+/3.3+ Application
+
+This application consists of a HTTP 1.1 server using the HTTP chunked transfer
+coding (https://tools.ietf.org/html/rfc2616#section-3.6.1) and a minimal HTML5
+user interface that interacts with it.
+
+The goal of this example is to start streaming the speech to the client (the
+HTML5 web UI) as soon as the first consumable chunk of speech is returned in
+order to start playing the audio as soon as possible.
+For use cases where low latency and responsiveness are strong requirements,
+this is the recommended approach.
+
+The service documentation contains examples for non-streaming use cases where
+waiting for the speech synthesis to complete and fetching the whole audio stream
+at once are an option.
+
+To test the application, run 'python server.py' and then open the URL
+displayed in the terminal in a web browser (see index.html for a list of
+supported browsers). The address and port for the server can be passed as
+parameters to server.py. For more information, run: 'python server.py -h'
+"""
+from argparse import ArgumentParser
+from collections import namedtuple
+from contextlib import closing
+from io import BytesIO
+from json import dumps as json_encode
+import os
+import sys
+import time
+import generator
+import io
+
+if sys.version_info >= (3, 0):
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+    from socketserver import ThreadingMixIn
+    from urllib.parse import parse_qs
+else:
+    from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+    from SocketServer import ThreadingMixIn
+    from urlparse import parse_qs
+
+#from boto3 import Session
+#from botocore.exceptions import BotoCoreError, ClientError
+
+ResponseStatus = namedtuple("HTTPStatus",
+                            ["code", "message"])
+
+ResponseData = namedtuple("ResponseData",
+                          ["status", "content_type", "data_stream"])
+
+# Mapping the output format used in the client to the content type for the
+# response
+AUDIO_FORMATS = {"ogg_vorbis": "audio/ogg",
+                 "mp3": "audio/mpeg",
+                 "pcm": "audio/wave; codecs=1"}
+CHUNK_SIZE = 4096
+HTTP_STATUS = {"OK": ResponseStatus(code=200, message="OK"),
+               "BAD_REQUEST": ResponseStatus(code=400, message="Bad request"),
+               "NOT_FOUND": ResponseStatus(code=404, message="Not found"),
+               "INTERNAL_SERVER_ERROR": ResponseStatus(code=500, message="Internal server error")}
+PROTOCOL = "http"
+ROUTE_INDEX = "/index.html"
+#ROUTE_VOICES = "/voices"
+#ROUTE_READ = "/read"
+
+
+# Create a client using the credentials and region defined in the adminuser
+# section of the AWS credentials and configuration files
+#session = Session(profile_name="adminuser")
+#polly = session.client("polly")
+
+
+class HTTPStatusError(Exception):
+    """Exception wrapping a value from http.server.HTTPStatus"""
+
+    def __init__(self, status, description=None):
+        """
+        Constructs an error instance from a tuple of
+        (code, message, description), see http.server.HTTPStatus
+        """
+        super(HTTPStatusError, self).__init__()
+        self.code = status.code
+        self.message = status.message
+        self.explain = description
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """An HTTP Server that handle each request in a new thread"""
+    daemon_threads = True
+
+
+class ChunkedHTTPRequestHandler(BaseHTTPRequestHandler):
+    """"HTTP 1.1 Chunked encoding request handler"""
+    # Use HTTP 1.1 as 1.0 doesn't support chunked encoding
+    protocol_version = "HTTP/1.1"
+
+    def query_get(self, queryData, key, default=""):
+        """Helper for getting values from a pre-parsed query string"""
+        return queryData.get(key, [default])[0]
+
+    def do_GET(self):
+        """Handles GET requests"""
+
+        # Extract values from the query string
+        path, _, query_string = self.path.partition('?')
+        query = parse_qs(query_string)
+
+        response = None
+
+        print(u"[START]: Received GET for %s with query: %s" % (path, query))
+
+        try:
+            # Handle the possible request paths
+            if path == ROUTE_INDEX:
+                response = self.route_index(path, query)
+            else:
+                response = self.route_not_found(path, query)
+
+            self.send_headers(response.status, response.content_type)
+            self.stream_data(response.data_stream)
+
+        except HTTPStatusError as err:
+            # Respond with an error and log debug
+            # information
+            if sys.version_info >= (3, 0):
+                self.send_error(err.code, err.message, err.explain)
+            else:
+                self.send_error(err.code, err.message)
+
+            self.log_error(u"%s %s %s - [%d] %s", self.client_address[0],
+                           self.command, self.path, err.code, err.explain)
+
+        print("[END]")
+
+    def route_not_found(self, path, query):
+        """Handles routing for unexpected paths"""
+        raise HTTPStatusError(HTTP_STATUS["NOT_FOUND"], "Page not found")
+
+    def route_index(self, path, query):
+        """Handles routing for the application's entry point'"""
+        result = generator.generate_samples('./', ['index.html'])
+        output = io.BytesIO(bytearray(result, 'utf8'))
+        try:
+            return ResponseData(status=HTTP_STATUS["OK"], content_type="text/html",
+                                # Open a binary stream for reading the index
+                                # HTML file
+                                data_stream=output)
+        except IOError as err:
+            # Couldn't open the stream
+            raise HTTPStatusError(HTTP_STATUS["INTERNAL_SERVER_ERROR"],
+                                  str(err))
+
+    def send_headers(self, status, content_type):
+        """Send out the group of headers for a successful request"""
+        # Send HTTP headers
+        self.send_response(status.code, status.message)
+        self.send_header('Content-type', content_type)
+        self.send_header('Transfer-Encoding', 'chunked')
+        self.send_header('Connection', 'close')
+        self.end_headers()
+
+    def stream_data(self, stream):
+        """Consumes a stream in chunks to produce the response's output'"""
+        print("Streaming started...")
+
+        if stream:
+            # Note: Closing the stream is important as the service throttles on
+            # the number of parallel connections. Here we are using
+            # contextlib.closing to ensure the close method of the stream object
+            # will be called automatically at the end of the with statement's
+            # scope.
+            with closing(stream) as managed_stream:
+                # Push out the stream's content in chunks
+                while True:
+                    data = managed_stream.read(CHUNK_SIZE)
+                    dataToWrite = b"%X\r\n%s\r\n" % (len(data), data)
+                    self.wfile.write(dataToWrite)
+                    #print(dataToWrite)
+
+                    # time.sleep(0.5)
+
+                    # If there's no more data to read, stop streaming
+                    if not data:
+                        break
+
+                # Ensure any buffered output has been transmitted and close the
+                # stream
+                self.wfile.flush()
+
+            print("Streaming completed.")
+        else:
+            # The stream passed in is empty
+            self.wfile.write(b"0\r\n\r\n")
+            print("Nothing to stream.")
+
+
+# Define and parse the command line arguments
+cli = ArgumentParser(description='Example Python Application')
+cli.add_argument(
+    "-p", "--port", type=int, metavar="PORT", dest="port", default=8000)
+cli.add_argument(
+    "--host", type=str, metavar="HOST", dest="host", default="0.0.0.0")
+arguments = cli.parse_args()
+
+# If the module is invoked directly, initialize the application
+if __name__ == '__main__':
+    # Create and configure the HTTP server instance
+    server = ThreadedHTTPServer((arguments.host, arguments.port),
+                                ChunkedHTTPRequestHandler)
+    print("Starting server, use <Ctrl-C> to stop...")
+    print(u"Open {0}://{1}:{2}{3} in a web browser.".format(PROTOCOL,
+                                                            arguments.host,
+                                                            arguments.port,
+                                                            ROUTE_INDEX))
+
+    try:
+        # Listen for requests indefinitely
+        server.serve_forever()
+    except KeyboardInterrupt:
+        # A request to terminate has been received, stop the server
+        print("\nShutting down...")
+        server.socket.close()
